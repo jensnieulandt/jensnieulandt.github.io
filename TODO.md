1d992ba43c2e6e4edbf3c3a3e630f70c0ec53262
Jens Nieulandt <jensnieulandt@gmail.com>
2023-01-22 16:47:22 +0100

    feat: add favicon and optimized meta tags

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 
 - [X] add favicon
 - [X] create a custom Nuxt theme inspired by content-wind
-- [ ] optimize meta tags
+- [X] optimize meta tags
 - [ ] look for a way to use prettier with mdc syntax
 - [X] add Linkedin connection
 - [X] make all external links open in new tab
--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -7,6 +7,11 @@ const { cover } = useAppConfig()
     <Html lang="en" />
     <Meta property="og:image" :content="cover" />
     <Meta name="twitter:card" content="summary_large_image" />
+    <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <Link rel="icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="manifest" href="/site.webmanifest" />
     <AppLoadingBar />
     <AppNavbar />
     <slot />
