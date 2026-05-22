3d1a15607f4e14c43d23bcf2ff15cd6e2501e59d
Jens Nieulandt <jens.nieulandt@inetum-realdolmen.world>
2026-05-23 01:11:16 +0200

    fix: pinceau import should use a stable path

--- shims/pinceau.ts
+++ shims/pinceau.ts
@@ -1,4 +1,4 @@
-export * from '../node_modules/.pnpm/pinceau@0.18.10_postcss@8.5.15_vue-tsc@2.2.12_typescript@5.9.3_/node_modules/pinceau/dist/index.mjs'
+export * from '../node_modules/pinceau'
 
 // Runtime placeholders for type-only imports emitted by current Pinceau generation.
 export const PinceauTheme = {}
